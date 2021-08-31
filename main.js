7028887d3de998fd453c23b5dd22109d1aa93a2a
Ashlyn423 <79730308+Ashlyn423@users.noreply.github.com>
2021-08-31 16:58:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+console.log("Hi, Im console screen")
+var firstname = "Ashlyn"
+var secondname = "Hermosillo"
+console.log(firstname)
+console.log(secondname)
+var fullname = firstname.concat(secondname)
+console.log(fullname)
+var len = fullname.length;
+console.log(len)
+var name1 = "I am Ashlyn Hermosillo";
+var rp = name1.replace("Ashlyn","Adryan")
+console.log(rp)
+var capitalname = "rocky"
+var makinglowercase = capitalname.toUpperCase()
+console.log(makinglowercase)
+var squareroot = Math.sqrt(5)
+console.log(squareroot)
+var min = Math.min(10,15)
+console.log(min)
+var power = Math.pow(5,2)
+console.log(power)
+var decimal = 3.11357532
+console.log(decimal)
+var y = decimal.toFixed(2)
+console.log(y)
+var num1 = 12
+var num2 = 13
+console.log(num1 + num2)
+var num1string = num1.toString()
+var num2string = num2.toString()
+console.log(num1string + num2string)
+var i = 10
+i++
+console.log(i)
+i--
+console.log(i)
+console.log(typeof num1)
+console.log(typeof num1string)
